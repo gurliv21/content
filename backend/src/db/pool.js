799f27a3d68b67a4916b0e137dd86dc6a4bd181e
gurlivkaurbajwa <gurlivbajwa@gmail.com>
2025-12-06 00:03:55 +0530

    fixed pool

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -3,16 +3,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 const { Pool } = pkg;
 
-const isProduction = process.env.NODE_ENV === "production";
+// const isProduction = process.env.NODE_ENV === "production";
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: isProduction
-    ? { rejectUnauthorized: false } 
-    : false
+  ssl: false
 });
 console.log("DB_NAME:", process.env.DB_NAME);
 
